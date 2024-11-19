8da36f306df726c91ba76d626a9af9ac444b0ebf
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-11-18 22:01:29 -0800

    changed character names, and some images. Took away background to test working javascript

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
-document.getElementById('purple').onclick = partyPurple
+document.getElementById('#player1').onclick = runPlayer1
 document.getElementById('green').onclick = partyGreen
 document.getElementById('blue').onclick = partyBlue
 document.getElementById('red').onclick = partyRed
 
 
-function partyPurple() {
+function runPlayer1() {
   document.querySelector('body').style.backgroundColor = 'rgba(241,63,247,1)'
   document.querySelector('body').style.color = 'white'
 }
