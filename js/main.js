758504d9ff5e00164f929ed455f6f842719576a9
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-11-25 10:13:51 -0800

    worked how to play video one and two, but they are not aligned

--- js/main.js
+++ js/main.js
@@ -5,3 +5,11 @@ function one(){
   document.getElementById('playVidPlayer1').play();
   console.log('oneWorks')
 } 
+
+
+function two(){
+  // document.getElementsByClassName('videoChar').classList.add('hide');
+  //   document.getElementById('playVidPlayer1').classList.remove('hide');
+    document.getElementById('playVidPlayer2').play();
+    console.log('twoWorks')
+  } 
