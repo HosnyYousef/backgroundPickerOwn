a578bdf3eff0518ae4fab68f37cea84f135e3faa
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-11-26 09:44:12 -0800

    got player1 video to work on click

--- js/main.js
+++ js/main.js
@@ -1,12 +1,42 @@
 
 
-function one(){
-  document.getElementById('playVidPlayer1').play();
-  console.log('oneWorks')
+function display(playVidPlayer){
+  hideAndStopAll()
+  var vid = document.getElementById(playVidPlayer);
+  vid.style.display = 'block';
+  // vid.play();
+document.getElementById('playVidPlayer1').play();
 } 
+var ids= ['playVidPlayer1','playVidPlayer2','playVidPlayer3','playVidPlayer4','playVidPlayer5','playVidPlayer6']
+
+function hideAndStopAll(){
+
+    for(i=0;i<ids.length;i++){
+        var video = document.getElementById(ids[i]);
+        video.pause();
+        video.style.display='none'
+        console.log('oneWorks')
+    }
+}
 
 
-function two(){
-    document.getElementById('playVidPlayer2').play();
-    console.log('twoWorks')
-  } 
+
+// function display(playVidPlayer){
+//   hideAndStopAll()
+//   var vid = document.getElementById(playVidPlayer);
+//   vid.style.display = 'block';
+//   // vid.play();
+// document.getElementById('playVidPlayer2').play();
+// } 
+// var ids= ['playVidPlayer1','playVidPlayer2','playVidPlayer3','playVidPlayer4','playVidPlayer5','playVidPlayer6']
+
+// function hideAndStopAll(){
+
+//     for(i=0;i<ids.length;i++){
+//         var video = document.getElementById(ids[i]);
+//         video.pause();
+//         video.style.display='none'
+//         console.log('twoWorks')
+//     }
+// }
+
