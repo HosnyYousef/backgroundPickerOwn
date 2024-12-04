87cd57c813e7d51666e829a1bbd3492b40508194
Yousef Hosny <Yousefhosny.hm@gmail.com>
2024-12-04 10:07:48 -0800

    trying to hide the default background before characters appear

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@
   
   
   function hideAndStopAll(videoIdx){
-  let ids = ['playVidPlayer1', 'playVidPlayer2', 'playVidPlayer3', 'playVidPlayer4', 'playVidPlayer5', 'playVidPlayer6', 'playVidPlayer7', 'playVidPlayer8', 'playVidPlayer9', 'playVidPlayer10', 'playVidPlayer11', 'playVidPlayer12', 'playVidPlayer13', 'playVidPlayer14', 'playVidPlayer15', 'playVidPlayer16', 'playVidPlayer17', 'playVidPlayer18']
+  let ids = ['playVidPlayer1', 'playVidPlayer2', 'playVidPlayer3', 'playVidPlayer4', 'playVidPlayer5', 'playVidPlayer6', 'playVidPlayer7', 'playVidPlayer8', 'playVidPlayer9', 'playVidPlayer10', 'playVidPlayer11', 'playVidPlayer12', 'playVidPlayer13', 'playVidPlayer14', 'playVidPlayer15', 'playVidPlayer16', 'playVidPlayer17', 'playVidPlayer18', 'playVidPlayer0']
 
     for (let i = 0; i < ids.length; i++) {
         let video = document.getElementById(ids[i]);
